2efba8f465ac7f92c923a8c0e7986f6675068d3e
hoda28 <99mrady@gmail.com>
2023-01-29 12:39:49 +0900

    created base Form

---
+++ TodoApp/MainForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace TodoApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnManage = new System.Windows.Forms.Button();
+            this.btnComp = new System.Windows.Forms.Button();
+            this.btnPending = new System.Windows.Forms.Button();
+            this.btnAll = new System.Windows.Forms.Button();
+            this.panelTitleBar = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(66)))));
+            this.panelMenu.Controls.Add(this.btnManage);
+            this.panelMenu.Controls.Add(this.btnComp);
+            this.panelMenu.Controls.Add(this.btnPending);
+            this.panelMenu.Controls.Add(this.btnAll);
+            this.panelMenu.Location = new System.Drawing.Point(0, 33);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(300, 996);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // btnManage
+            // 
+            this.btnManage.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnManage.FlatAppearance.BorderSize = 0;
+            this.btnManage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnManage.Font = new System.Drawing.Font("Orbitron", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnManage.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnManage.Location = new System.Drawing.Point(0, 282);
+            this.btnManage.Name = "btnManage";
+            this.btnManage.Size = new System.Drawing.Size(300, 94);
+            this.btnManage.TabIndex = 4;
+            this.btnManage.Text = "   Manage Categories";
+            this.btnManage.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnManage.UseVisualStyleBackColor = true;
+            this.btnManage.Click += new System.EventHandler(this.btnManage_Click);
+            // 
+            // btnComp
+            // 
+            this.btnComp.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnComp.FlatAppearance.BorderSize = 0;
+            this.btnComp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnComp.Font = new System.Drawing.Font("Orbitron", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnComp.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnComp.Location = new System.Drawing.Point(0, 188);
+            this.btnComp.Name = "btnComp";
+            this.btnComp.Size = new System.Drawing.Size(300, 94);
+            this.btnComp.TabIndex = 3;
+            this.btnComp.Text = "   Completed";
+            this.btnComp.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnComp.UseVisualStyleBackColor = true;
+            this.btnComp.Click += new System.EventHandler(this.btnComp_Click);
+            // 
+            // btnPending
+            // 
+            this.btnPending.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPending.FlatAppearance.BorderSize = 0;
+            this.btnPending.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPending.Font = new System.Drawing.Font("Orbitron", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPending.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnPending.Location = new System.Drawing.Point(0, 94);
+            this.btnPending.Name = "btnPending";
+            this.btnPending.Size = new System.Drawing.Size(300, 94);
+            this.btnPending.TabIndex = 2;
+            this.btnPending.Text = "   Pending";
+            this.btnPending.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPending.UseVisualStyleBackColor = true;
+            this.btnPending.Click += new System.EventHandler(this.btnPending_Click);
+            // 
+            // btnAll
+            // 
+            this.btnAll.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAll.FlatAppearance.BorderSize = 0;
+            this.btnAll.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAll.Font = new System.Drawing.Font("Orbitron", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAll.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnAll.Location = new System.Drawing.Point(0, 0);
+            this.btnAll.Name = "btnAll";
+            this.btnAll.Size = new System.Drawing.Size(300, 94);
+            this.btnAll.TabIndex = 1;
+            this.btnAll.Text = "   All";
+            this.btnAll.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAll.UseVisualStyleBackColor = true;
+            this.btnAll.Click += new System.EventHandler(this.btnAll_Click);
+            // 
+            // panelTitleBar
+            // 
+            this.panelTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(23)))), ((int)(((byte)(68)))));
+            this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitleBar.Location = new System.Drawing.Point(0, 0);
+            this.panelTitleBar.Name = "panelTitleBar";
+            this.panelTitleBar.Size = new System.Drawing.Size(800, 36);
+            this.panelTitleBar.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.panelTitleBar);
+            this.Controls.Add(this.panelMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.panelMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button btnManage;
+        private System.Windows.Forms.Button btnComp;
+        private System.Windows.Forms.Button btnPending;
+        private System.Windows.Forms.Button btnAll;
+        private System.Windows.Forms.Panel panelTitleBar;
+    }
+}
+
---
+++ TodoApp/MainForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TodoApp
+{
+    public partial class MainForm : Form
+    {
+
+        #region Fields
+        private Button currentButton;
+        #endregion
+
+        #region Constructor
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Private Methods
+        private void ActivateButton(object btnSender)
+        { 
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    Color color = Color.FromArgb(255, 23, 68);
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = color;
+                    currentButton.ForeColor = Color.FromArgb(245, 245, 245);
+                    currentButton.Font = new System.Drawing.Font("Orbitron", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }
+            }
+        }
+
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in panelMenu.Controls)
+            {
+                previousBtn.BackColor = Color.FromArgb(66, 66, 66);
+                previousBtn.ForeColor = Color.WhiteSmoke;
+                previousBtn.Font = new System.Drawing.Font("Orbitron", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            }
+        }
+
+        #region Panel Menu
+        #region btnAll_Click()
+        private void btnAll_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+        #endregion
+
+        #region btnPending_Click()
+        private void btnPending_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+        #endregion
+
+        #region btnComp_Click()
+        private void btnComp_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+        #endregion
+
+        #region btnManage_Click()
+        private void btnManage_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+        #endregion
+        #endregion
+
+        #endregion
+
+    }
+}
